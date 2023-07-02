4472f21d6404a6e7a45a54718435e84a9be60cfa
themrsung <themrsung@gmail.com>
2023-07-02 18:24:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# OasisServer
+#### "The" best Minecraft economy server.
+
+Uses EconomyX.
+https://github.com/themrsung/EconomyX
+
+This is an example implementation of EconomyX, as well as a representation of how EconomyX was designed to be used.
+
+This is the actual base code of a running Minecraft server.
+
+This project is open source purely for the purpose of giving an example on how to implement EconomyX into a usable product. 
+**You may not use this code freely.** (See [LICENSE](LICENSE))
---
+++ src/main/java/oasis/oasisserver/OasisServer.java
@@ -0,0 +1,63 @@
+package oasis.oasisserver;
+
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import oasis.economyx.EconomyX;
+
+/**
+ * Main class of OasisServer
+ */
+public final class OasisServer extends EconomyX {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+        // ProtocolLib
+        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+
+
+
+        // as#uuid#(whatever)
+
+        // create#company#symbol#name#capital
+        // create#organization#name#currency
+        // create#nation#name#capital
+
+        // bank#deposit#bankuuid#accountuuid#amount
+        // bank#withdraw#bankuuid#accountuuid#amount
+
+        // card#issue
+        // card#activate
+        // card#revoke
+        // card#terminal#create
+
+        // contract#create
+        // contract#forgive
+
+        // guarantee#issue
+        // guarantee#revoke
+
+        // pay#recipient#amount
+
+        // property#claim
+        // property#abandon
+        // property#setprotector (called by holder)
+        // property#authorizeprotection (called by protector)
+
+        // market#list#listing
+        // market#delist#listing
+
+        // order#create#order
+        // order#cancel#order
+
+        // vault#create
+        // vault#setkeeper (called by holder)
+        // vault#authroizekeeping (called by keeper)
+
+        // vote#create
+        // vote#cast#votes
+
+        // hostility#declare
+        // hostility#revoke
+    }
+}
